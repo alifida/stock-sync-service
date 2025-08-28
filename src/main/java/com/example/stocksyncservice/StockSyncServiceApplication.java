4f5d8aa1e8b1cd39b9e5a6c9b97f6af46143339a
Ali Fida <alifida.86@gmail.com>
2025-08-28 23:03:29 +0500

    Update .gitattributes, .gitignore, and 11 more files...

---
+++ src/main/java/com/example/stocksyncservice/StockSyncServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.stocksyncservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSyncServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSyncServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stocksyncservice/StockSyncServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stocksyncservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSyncServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
